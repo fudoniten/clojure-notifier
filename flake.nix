6857e3bd36e831775e350fc5a2e70c3d47991471
niten <niten@fudo.org>
2023-05-10 10:49:39 -0700

    Add flake.nix for lib

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Library for sending DBus notifications.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+    utils.url = "github:numtide/flake-utils";
+    helpers = {
+      url = "git+https://git.fudo.org/fudo-public/nix-helpers.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, helpers, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        packages = rec {
+          default = notifier;
+          notifier = helpers.packages."${system}".mkClojureLib {
+            name = "org.fudo/notifier";
+            src = ./.;
+          };
+        };
+
+        devShells = rec {
+          default = updateDeps;
+          updateDeps = pkgs.mkShell {
+            buildInputs = with helpers.packages."${system}";
+              [ updateClojureDeps ];
+          };
+        };
+      });
+}
